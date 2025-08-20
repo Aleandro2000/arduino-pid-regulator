42fd2df37f3fccbefd9f0183beb0136d4e105ba9
Aleandro2000 <carmicialexandruandrei@gmail.com>
2025-08-20 15:45:46 +0300

    modification

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,44 +2,42 @@
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include <math.h>
-#include <AccelStepper.h>
 #include <PID_v1_bc.h>
 
 #define MODULATION_RATE 9600
 #define DHT_PIN 7
 #define LED_PIN 8
-#define STEPPER_PIN1 4
-#define STEPPER_PIN2 5
-#define STEPPER_PIN3 6
-#define STEPPER_PIN4 9
+#define MOTOR_PIN_IN1 A1
+#define MOTOR_PIN_IN2 A2
+#define MOTOR_PIN_EN A0
 
 dht DHT;
 SoftwareSerial BT(2, 3);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-AccelStepper stepper(AccelStepper::FULL4WIRE, STEPPER_PIN1, STEPPER_PIN3, STEPPER_PIN2, STEPPER_PIN4);
 
 double tempSetpoint = 24.0;
-double humSetpoint = 56.0;
-double inputVal, outputFan;
-PID fanPID(&inputVal, &outputFan, &tempSetpoint, 2.0, 5.0, 1.0, P_ON_E, DIRECT);
+double inputVal, outputMotor;
+
+PID motorPID(&inputVal, &outputMotor, &tempSetpoint, 2.0, 5.0, 1.0, DIRECT);
 
 unsigned long lastUpdate = 0;
+unsigned long lastDHT = 0;
 
 void setup() {
   Serial.begin(MODULATION_RATE);
   BT.begin(MODULATION_RATE);
   pinMode(LED_PIN, OUTPUT);
+  pinMode(MOTOR_PIN_IN1, OUTPUT);
+  pinMode(MOTOR_PIN_IN2, OUTPUT);
+  pinMode(MOTOR_PIN_EN, OUTPUT);
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
   lcd.print("System Ready");
   delay(1000);
   lcd.clear();
-  stepper.setMaxSpeed(4000);
-  stepper.setAcceleration(2000);
-  fanPID.SetMode(AUTOMATIC);
-  fanPID.SetOutputLimits(500, 4000);
+  motorPID.SetMode(AUTOMATIC);
+  motorPID.SetOutputLimits(0, 255);
 }
 
 void loop() {
@@ -48,26 +46,22 @@ void loop() {
     digitalWrite(LED_PIN, cmd == '1' ? HIGH : LOW);
     Serial.println(cmd == '1' ? "LED On" : "LED Off");
   }
-
-  if (DHT.read11(DHT_PIN) == DHTLIB_OK) {
-    float temp = DHT.temperature;
-    float hum = DHT.humidity;
-    double tempError = abs(temp - tempSetpoint);
-    double humError = abs(hum - humSetpoint);
-    inputVal = (humError > tempError) ? hum : temp;
-    tempSetpoint = (humError > tempError) ? humSetpoint : 24.0;
-    fanPID.Compute();
-    stepper.setSpeed(outputFan);
+  if (millis() - lastDHT > 1000) {
+    lastDHT = millis();
+    if (DHT.read11(DHT_PIN) == DHTLIB_OK) {
+      inputVal = DHT.temperature;
+      motorPID.Compute();
+      digitalWrite(MOTOR_PIN_IN1, HIGH);
+      digitalWrite(MOTOR_PIN_IN2, LOW);
+      analogWrite(MOTOR_PIN_EN, (int)outputMotor);
+    }
   }
-
-  stepper.runSpeed();
-
   if (millis() - lastUpdate > 100) {
     lastUpdate = millis();
-    BT.println("{\"temperature\":" + String(DHT.temperature, 2) + ",\"humidity\":" + String(DHT.humidity, 2) + ",\"fanSpeed\":" + String(outputFan, 0) + "}");
+    BT.println("{\"temperature\":" + String(DHT.temperature, 2) + ",\"humidity\":" + String(DHT.humidity, 2) + ",\"motorSpeed\":" + String(outputMotor, 0) + "}");
     lcd.setCursor(0, 0);
     lcd.print("T:" + String(DHT.temperature, 1) + "C H:" + String(DHT.humidity, 0) + "%   ");
     lcd.setCursor(0, 1);
-    lcd.print("FAN:" + String(outputFan, 0) + " SPD   ");
+    lcd.print("Motor:" + String(outputMotor, 0) + " SPD   ");
   }
 }
